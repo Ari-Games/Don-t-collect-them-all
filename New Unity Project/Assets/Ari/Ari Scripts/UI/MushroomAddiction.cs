2d5f624241edfcca4b98778f46c2302f04964521
Edcurrock <edgtg0304@gmail.com>
2020-10-31 00:45:16 +0300

    Add Mushroom Addiction (MAIN LOGIC!!!!)

---
+++ New Unity Project/Assets/Ari/Ari Scripts/UI/MushroomAddiction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MushroomAddiction : MonoBehaviour
+{
+
+    [SerializeField] float standartAddictionValue = 0.5f;
+    [SerializeField] float addictionSpeed = 0.2f;
+    [SerializeField] Image addictionImage;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(addictionImage.fillAmount >= 0.95f || addictionImage.fillAmount <= 0.05f)
+        {
+            //TODO GAMEOVER
+            print("GAME OVER");
+            return;
+        }
+        addictionImage.fillAmount -=Time.deltaTime * addictionSpeed;
+        
+    }
+
+    private void OnEnable()
+    {
+        addictionImage.fillAmount = standartAddictionValue;
+    }
+
+    public void AddAddiction(float value)
+    {
+        addictionImage.fillAmount = value;
+    }
+}
