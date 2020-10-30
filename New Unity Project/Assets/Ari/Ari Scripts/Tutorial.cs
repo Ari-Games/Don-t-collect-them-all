8ff1c313ef4c5528a6126f577c8117ef2d8ab0b0
Alisher <fazilov.99@mail.ru>
2020-10-31 01:23:12 +0300

    Added fonts, added tutorial, update canvas

---
+++ New Unity Project/Assets/Ari/Ari Scripts/Tutorial.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private Text task;
+    [SerializeField] private string textTask;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            task.text = textTask;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && GWorld.IsOurWorld())
+            Destroy(this.gameObject);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            task.text = "";
+    }
+}
