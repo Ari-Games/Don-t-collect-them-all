37d6068a16f272ef26bbd36ce64016fd7e9318e4
Aramcho <osipyan.aram2017@ya.ru>
2020-10-30 17:44:23 +0300

    Add fireball shooting

---
+++ New Unity Project/Assets/Scripts/FireEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEnemy : Enemy
+{
+    float timeOf;
+    bool _shootTrigger = false;
+
+    GameObject _target;
+    [SerializeField] GameObject FireBall;
+    [SerializeField] float frequency = 1f;
+    void Start()
+    {
+        timeOf = Time.time;
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision +" "+ collision.tag);
+        if (collision.tag!="Player")
+        {
+            return;
+        }
+        print("fireball");
+
+        _target = collision.gameObject;
+        _shootTrigger = true;
+
+    }
+    void Update()
+    {
+        if (_shootTrigger && (timeOf)>= frequency)
+        {
+            var ball = Instantiate(FireBall, transform.position, Quaternion.identity).GetComponent<Fireball>();
+            Destroy(ball.gameObject, 5f);
+            var to = (_target.transform.position - transform.position);
+            to.y += 1;
+            ball.DirectTo(to, 5);
+            timeOf = 0;
+        }
+        timeOf += Time.deltaTime;
+    }
+    public override void Death()
+    {
+        Destroy(this.gameObject);
+    }
+    
+}
---
+++ New Unity Project/Assets/Scripts/Fireball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    Rigidbody2D _rigidbody2D;
+    Vector3 _direction;
+    int _force = 0;
+
+    [SerializeField] int Speed = 100;
+    [SerializeField] GameObject SpriteOfBall;
+    
+    void Start()
+    {
+        //_rigidbody2D = GetComponent<Rigidbody2D>();
+       
+    }
+    private void FixedUpdate()
+    {
+        //_rigidbody2D.AddForce(_direction.normalized * _force);
+    }
+    public void DirectTo(Vector3 direction,int force)
+    {
+        _direction = direction;
+        _force = force;
+        
+    }
+    void Update()
+    {
+        //transform.LookAt(_direction * Time.deltaTime);
+        transform.Translate(_direction.normalized*Speed * Time.deltaTime);
+        SpriteOfBall.transform.LookAt(_direction);
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player")
+        {
+            return;
+        }
+        print("BOOM");
+    }
+}
