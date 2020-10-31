92b563437682170ea9f52478b85641d45d75bf82
Alisher <fazilov.99@mail.ru>
2020-10-31 16:59:24 +0300

    Added main menu

---
+++ New Unity Project/Assets/Ari/Ari Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void GoToPlay()
+    {
+        Debug.Log(10);
+        SceneManager.LoadScene(1);
+    }
+}
