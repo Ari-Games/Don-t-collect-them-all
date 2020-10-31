7415a749bb1740fddc0db7ee69eefb5c7168f853
Edcurrock <edgtg0304@gmail.com>
2020-10-31 21:46:37 +0300

    For Alisher

---
+++ New Unity Project/Assets/Ari/Ari Scripts/UI/BookScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BookScene : MonoBehaviour
+{
+    public void OnApply()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
